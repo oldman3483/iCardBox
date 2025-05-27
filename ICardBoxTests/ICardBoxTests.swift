7cca66c636d011a9918db98c6420d9c1d20eb22f
Heidie1121 <heidielee1121@gmail.com>
2025-05-27 16:13:26 +0800

    First Version

---
+++ ICardBoxTests/ICardBoxTests.swift
@@ -0,0 +1,17 @@
+//
+//  ICardBoxTests.swift
+//  ICardBoxTests
+//
+//  Created by Heidie Lee on 2025/5/27.
+//
+
+import Testing
+@testable import ICardBox
+
+struct ICardBoxTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
